84fcff132e99f9b9331736c0c5940ae2e1db7eea
mcflugen <mcflugen@gmail.com>
2020-10-21 17:20:58 -0600

    fix formatting

--- pymt_sedflux/lib/__init__.py
+++ pymt_sedflux/lib/__init__.py
@@ -1,6 +1,9 @@
 #! /usr/bin/env python
 
-from .sedflux3d import Sedflux3Dfrom .avulsion import Avulsionfrom .plume import Plumefrom .subside import Subside
+from .sedflux3d import Sedflux3D
+from .avulsion import Avulsion
+from .plume import Plume
+from .subside import Subside
 
 __all__ = ["Sedflux3D","Avulsion","Plume","Subside",
 ]
